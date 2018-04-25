0f6dbd878fe5e1d7b525f9813434890af4bedab5
franco-extreme <franck_ral@hotmail.com>
2018-04-25 12:33:16 -0400

    configure-config-server: Applying initial configurations to the config server.

--- src/main/java/com/fral/uo/config/uoconfigservice/UoConfigServiceApplication.java
+++ src/main/java/com/fral/uo/config/uoconfigservice/UoConfigServiceApplication.java
@@ -2,7 +2,9 @@ package com.fral.uo.config.uoconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class UoConfigServiceApplication {
 
