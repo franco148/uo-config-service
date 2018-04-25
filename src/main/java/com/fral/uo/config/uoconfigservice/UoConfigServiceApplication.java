aec155098ecc53118e785726c795906f87137867
franco-extreme <franck_ral@hotmail.com>
2018-04-24 22:25:52 -0400

    Initial status of the project UO Configuration Service.

---
+++ src/main/java/com/fral/uo/config/uoconfigservice/UoConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package com.fral.uo.config.uoconfigservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UoConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UoConfigServiceApplication.class, args);
+	}
+}
